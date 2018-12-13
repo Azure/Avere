a31fd1e34585bb5c540597765de9e3bbb65194b3
Anthony Howe <ahowe_ca@yahoo.ca>
2018-12-13 16:16:48 -0500

    fix bug

--- src/go/pkg/azure/blobcontainer.go
+++ src/go/pkg/azure/blobcontainer.go
@@ -177,8 +177,10 @@ func InitializeBlobContainer(ctx context.Context, storageAccount string, storage
 
 // UploadBlob uploads the blob to the container
 func (b *BlobContainer) UploadBlob(blobname string, data []byte) error {
+	start := time.Now()
 	log.Info.Printf("[Upload Bob %s", blobname)
-	defer log.Info.Printf("Upload Bob %s]", blobname)
+	defer log.Info.Printf("Upload Bob %s (delta %v)]", blobname, time.Now().Sub(start))
+
 	blobURL := b.ContainerURL.NewBlockBlobURL(blobname)
 	if _, err := azblob.UploadBufferToBlockBlob(b.Context, data, blobURL, azblob.UploadToBlockBlobOptions{}); err != nil {
 		log.Error.Printf("encountered error uploading blob '%s': '%v'", blobname, err)
