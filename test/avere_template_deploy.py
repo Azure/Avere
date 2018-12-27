f9f2aa2348091b7def358ae02ab3b18b46e9fcd7
Omar Zevallos <45883639+omzevall@users.noreply.github.com>
2018-12-27 15:22:03 -0500

    Use Azure SDK instead of building command-line calls

--- test/avere_template_deploy.py
+++ test/avere_template_deploy.py
@@ -20,39 +20,54 @@ import os
 import random
 import subprocess
 import sys
+import time
 from urllib.request import urlretrieve
 from string import ascii_lowercase, digits
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.resources.models import DeploymentMode
 
 # GLOBAL VARIABLES ############################################################
 
-AZ_PARAMS = {}
 DEFAULT_LOCATION = 'eastus2'
+DEPLOY_PARAMS = {}
 RESOURCE_GROUP_CREATED = False
 SCRIPT_ARGS = None
-SECRETS_FILE = None
 TEMPLATE_URL = 'https://raw.githubusercontent.com/Azure/Avere/master/src/vfxt/azuredeploy-auto.json'
 TEMPLATE_LOCAL_FILE = os.path.basename(TEMPLATE_URL)
 
 # FUNCTIONS ###################################################################
 
+def load_credentials():
+    """Loads Azure credentials from environment variables."""
+    print('> Load Azure credentials.')
+    return ResourceManagementClient(
+        credentials=ServicePrincipalCredentials(
+            client_id=os.environ['AZURE_CLIENT_ID'],
+            secret=os.environ['AZURE_CLIENT_SECRET'],
+            tenant=os.environ['AZURE_TENANT_ID']
+        ),
+        subscription_id=os.environ['AZURE_SUBSCRIPTION_ID']
+    )
+
 def load_params():
     """
     Loads the parameters needed in this script (e.g., resource group name).
 
-    If the user specified a parameters file, load those values into AZ_PARAMS.
+    If the user specified a parameters file, load those values into DEPLOY_PARAMS.
     Otherwise, generate the parameter values and store those values for re-use.
     The generated parameter values are stored in the current working directory
     as <resource-group-name>.params.json.
     """
-    global AZ_PARAMS
+    global DEPLOY_PARAMS
     if SCRIPT_ARGS.param_file:  # Open user-specified params file.
         with open(SCRIPT_ARGS.param_file) as config_file:
-            AZ_PARAMS = json.load(config_file)
+            DEPLOY_PARAMS = json.load(config_file)
     else:  # Generate and store params.
         random_id = 'av' + \
             ''.join(random.choice(ascii_lowercase + digits) for _ in range(6))
         rg_name = 'aapipe-' + random_id + '-rg'
-        AZ_PARAMS = {
+        DEPLOY_PARAMS = {
             'resource-group': rg_name,
             'parameters': {
                 'virtualNetworkResourceGroup': rg_name,
@@ -63,110 +78,104 @@ def load_params():
                 'controllerAuthenticationType': 'password'
             }
         }
-        with open(AZ_PARAMS['resource-group'] + '.params.json', 'w') as _pfile:
-            json.dump(AZ_PARAMS, _pfile)
-    _debug('AZ_PARAMS: {}'.format(AZ_PARAMS))
+        with open(DEPLOY_PARAMS['resource-group'] + '.params.json', 'w') as pf:
+            json.dump(DEPLOY_PARAMS, pf)
 
     # Set location/region. Precedence: command-line > params file > default
     if not SCRIPT_ARGS.location:
-        SCRIPT_ARGS.location = AZ_PARAMS.pop('location', DEFAULT_LOCATION)
+        SCRIPT_ARGS.location = DEPLOY_PARAMS.pop('location', DEFAULT_LOCATION)
+    _debug('SCRIPT_ARGS.location = {}'.format(SCRIPT_ARGS.location))
+    _debug('DEPLOY_PARAMS (before secrets): {}'.format(DEPLOY_PARAMS))
 
-def load_secrets():
-    """
-    Loads secrets from environment variables and stores them in a JSON file.
-    Before this script terminates, the generated secrets file is deleted.
-    """
-    exp_envars = [
-        'controllerPassword',
-        'adminPassword',
-        'servicePrincipalTenant',
-        'servicePrincipalAppId',
-        'servicePrincipalPassword'
-    ]
-    data = {'parameters': {}}
-    try:
-        for ev in exp_envars:
-            data['parameters'][ev] = {'value': os.environ[ev]}
-    except KeyError:
-        raise Exception('The following envars must be defined: ' +
-                        ', '.join(exp_envars))
+    # Add secrets to the parameters for template deployment.
+    secrets = {
+        'adminPassword': os.environ['AVERE_ADMIN_PW'],
+        'controllerPassword': os.environ['AVERE_CONTROLLER_PW'],
+        'servicePrincipalAppId': os.environ['AZURE_CLIENT_ID'],
+        'servicePrincipalPassword': os.environ['AZURE_CLIENT_SECRET'],
+        'servicePrincipalTenant': os.environ['AZURE_TENANT_ID']
+    }
+    DEPLOY_PARAMS['parameters'] = { **DEPLOY_PARAMS['parameters'], **secrets }
 
-    global SECRETS_FILE
-    SECRETS_FILE = AZ_PARAMS['resource-group'] + '.secrets.json'
-    with open(SECRETS_FILE, 'w') as params:
-            json.dump(data, params)
-
-def download_template():
+def load_template():
     """Downloads the Avere vFXT deployment template."""
     print('> Downloading template: ' + TEMPLATE_URL)
     urlretrieve(TEMPLATE_URL, filename=TEMPLATE_LOCAL_FILE)
+    with open(TEMPLATE_LOCAL_FILE, 'r') as template_file_fd:
+        template = json.load(template_file_fd)
+    return template
 
-def create_resource_group():
+def create_resource_group(rm_client):
     """
     Creates an Azure resource group.
 
     Assumes that the invoker has already used "az login" to authenticate.
     """
     global RESOURCE_GROUP_CREATED
-    print('> Creating resource group: ' + AZ_PARAMS['resource-group'])
-    _run_az_cmd('az group create --name {0} --location {1} {2}'.format(
-                AZ_PARAMS['resource-group'], SCRIPT_ARGS.location,
-                '--debug' if SCRIPT_ARGS.az_debug else ''))
+    print('> Creating resource group: ' + DEPLOY_PARAMS['resource-group'])
+    if not SCRIPT_ARGS.skip_az_ops:
+        rg = rm_client.resource_groups.create_or_update(
+            DEPLOY_PARAMS['resource-group'],
+            { 'location': SCRIPT_ARGS.location }
+        )
+        _debug('Resource Group = {}'.format(rg))
     RESOURCE_GROUP_CREATED = True
 
-def deploy_template():
+def deploy_template(rm_client):
     """Deploys the Avere vFXT template."""
     print('> Deploying template')
-    cmd = """az group deployment create {0}
-    --resource-group {1}
-    --template-file {2}
-    --parameters @{3}""".format('--debug' if SCRIPT_ARGS.az_debug else '',
-                                AZ_PARAMS['resource-group'],
-                                TEMPLATE_LOCAL_FILE, SECRETS_FILE)
 
-    if AZ_PARAMS['parameters']:  # There are more parameters.
-        cmd += "\n\t--parameters"
-        for key, val in AZ_PARAMS['parameters'].items():
-            cmd += "\n\t\t{0}={1}".format(key, val)
+    # Prepare parameters.
+    parameters = {k: {'value': v} for k, v in DEPLOY_PARAMS['parameters'].items()}
 
-    _run_az_cmd(cmd)
+    if not SCRIPT_ARGS.skip_az_ops:
+        op = rm_client.deployments.create_or_update(
+            resource_group_name=DEPLOY_PARAMS['resource-group'],
+            deployment_name='avere-template-deploy-test',
+            properties={
+                'mode': DeploymentMode.incremental,
+                'template': load_template(),
+                'parameters': parameters
+            }
+        )
+        _wait_for_op(op)
 
-def delete_resource_group():
-    print('> Deleting resource group: ' + AZ_PARAMS['resource-group'])
-    _run_az_cmd('az group delete --yes --name {0} {1}'.format(
-                AZ_PARAMS['resource-group'],
-                '--debug' if SCRIPT_ARGS.az_debug else ''))
+def delete_resource_group(rm_client):
+    """Deletes the resource group"""
+    print('> Deleting resource group: ' + DEPLOY_PARAMS['resource-group'])
+    if not SCRIPT_ARGS.skip_az_ops:
+        op = rm_client.resource_groups.delete(DEPLOY_PARAMS['resource-group'])
+        _wait_for_op(op)
 
-def cleanup():
+def cleanup(rm_client):
     """
     Performs multiple cleanup activities.
-        1. Deletes the generated secrets file.
-        2. Deletes the downloaded template file.
-        3. Deletes the resource group.
+        1. Deletes the downloaded template file.
+        2. Deletes the resource group.
     """
     print('> Cleaning up')
-    if os.path.isfile(SECRETS_FILE):
-        os.remove(SECRETS_FILE)
-
     if os.path.isfile(TEMPLATE_LOCAL_FILE):
         os.remove(TEMPLATE_LOCAL_FILE)
 
     if not SCRIPT_ARGS.skip_rg_cleanup and RESOURCE_GROUP_CREATED:
-        delete_resource_group()
+        delete_resource_group(rm_client)
 
 # HELPER FUNCTIONS ############################################################
 
-def _run_az_cmd(_cmd):
-    """Runs the Azure (az) command in the shell."""
-    cmd = _cmd.strip()
-    _debug('az command: "' + cmd + '"')
+def _wait_for_op(op, timeout_sec=60):
+    """
+    Wait for a long-running operation (op) for timeout_sec seconds.
 
-    if SCRIPT_ARGS.print_az_cmds:
-        print('az command: "' + cmd + '"')
-    else:
-        cmd = cmd.split()
-        sys.stdout.flush()
-        subprocess.check_call(cmd, stderr=subprocess.STDOUT)
+    op is an AzureOperationPoller object.
+    """
+    time_start = time.time()
+    while not op.done():
+        op.wait(timeout=timeout_sec)
+        print('>> operation status: {0} ({1} sec)'.format(
+                op.status(), int(time.time() - time_start)))
+    result = op.result()
+    if result:
+        print('>> operation result: {}'.format(result))
 
 def _debug(s):
     """Prints the passed string, with a DEBUG header, if debug is on."""
@@ -177,24 +186,24 @@ def _debug(s):
 
 def main():
     """Main script driver."""
+    rm_client = load_credentials()
     load_params()
-    load_secrets()
     retcode = 0  # PASS
     try:
-        download_template()
-        create_resource_group()
-        deploy_template()
-    except:
-        print('><' * 40)
-        print('> ERROR: test failed')
-        print('><' * 40)
+        create_resource_group(rm_client)
+        deploy_template(rm_client)
+    except Exception as ex:
+        print('\n' + ('><' * 40))
+        print('> TEST FAILED')
+        print('> EXCEPTION TEXT: {}'.format(ex))
+        print(('><' * 40) + '\n')
         retcode = 1  # FAIL
         raise
     finally:
-        cleanup()
+        cleanup(rm_client)
 
     print('> SCRIPT COMPLETE. Resource Group: {} (region: {})'.format(
-          AZ_PARAMS['resource-group'], SCRIPT_ARGS.location))
+          DEPLOY_PARAMS['resource-group'], SCRIPT_ARGS.location))
     print('> RESULT: ' + ('FAIL' if retcode else 'PASS'))
     sys.exit(retcode)
 
@@ -203,18 +212,16 @@ if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
         description='Test template-based Avere vFXT deployment.')
 
-    arg_parser.add_argument('-pf', '--param-file', default=None,
+    arg_parser.add_argument('-p', '--param-file', default=None,
         help='Full path to JSON params file. ' +
              'Default: None (generate new params)')
     arg_parser.add_argument('-l', '--location', default=None,
         help='Azure location (region short name) to use for deployment. ' +
              'Default: ' + DEFAULT_LOCATION)
-    arg_parser.add_argument('-p', '--print-az-cmds', action='store_true',
-        help='Print "az" commands to STDOUT instead of running them.')
-    arg_parser.add_argument('-sc', '--skip-rg-cleanup', action='store_true',
-        help='Do not delete the resource group.')
-    arg_parser.add_argument('-ad', '--az-debug', action='store_true',
-        help='Turn on Azure (az) command debugging.')
+    arg_parser.add_argument('-xo', '--skip-az-ops', action='store_true',
+        help='Do NOT actually run any of the Azure operations.')
+    arg_parser.add_argument('-xc', '--skip-rg-cleanup', action='store_true',
+        help='Do NOT delete the resource group during cleanup.')
     arg_parser.add_argument('-d', '--debug', action='store_true',
         help='Turn on script debugging.')
     SCRIPT_ARGS = arg_parser.parse_args()
