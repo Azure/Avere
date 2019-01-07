89ec14a0c0d7c68209594398d2a7a8bd1138dba7
remakar <remakar@averesys.com>
2019-01-07 08:51:58 -0500

    add vdbench test which makes template

--- test/test_avere_template_deploy.py
+++ test/test_avere_template_deploy.py
@@ -11,25 +11,57 @@ import pytest
 from scp import SCPClient
 
 from avere_template_deploy import AvereTemplateDeploy
+from azure.mgmt.resource.resources.models import DeploymentMode, TemplateLink
 
-
+result = None
 # TEST CASES ##################################################################
 class TestDeployment:
     def test_deploy_template(self, atd, resource_group):
+        global result
         op = atd.deploy()
         try:
             wait_for_op(op)
         finally:
             result = op.result()
-            print('>> operation result: {}'.format(result))
-            if result:
-                print('>> result.properties: {}'.format(result.properties))
-                user, server = result.properties.outputs['ssH_STRING'].split('@')
-                ssh = createSSHClient(server, user)
-                scp = SCPClient(ssh.get_transport())
-                scp.get(r'~/vfxt.log', r'./vfxt.' + atd.resource_group + '.log')
+            # print('>> operation result: {}'.format(result))
+            # if result:
+            #     print('>> result.properties: {}'.format(result.properties))
+            #     user, server = result.properties.outputs['ssH_STRING'].split('@')
+            #     ssh = createSSHClient(server, user)
+            #     scp = SCPClient(ssh.get_transport())
+            #     scp.get(r'~/vfxt.log', r'./vfxt.' + atd.resource_group + '.log')
+    
+    def test_vdbench(self, atd):
+        with open(atd.ssh_pub_key_path, 'r') as ssh_pub_key_file:
+                 ssh_pub_key = ssh_pub_key_file.read()
+        
+        vserver = result.properties.outputs["vserveR_IPS"]["value"]
+        x = vserver.split("-")
+        ip1 = x[0]
+        ip2 = x[1]
+        ip1_split_list = ip1.split(".")
+        ip2_split_list = ip2.split(".")
+        outcome = ip1_split_list[-1]
+        outcome2 = ip2_split_list[-1]
 
+        prefix = ".".join(ip1_split_list[:-1])
+        prefix += "."
 
+        vserver_list = ",".join([prefix + str(n) for n in range(int(outcome), int(outcome2)+1)])
+        atd.template_link = TemplateLink(uri ="https://raw.githubusercontent.com/Azure/Avere/master/src/client/vmas/azuredeploy.json")
+        original_params = atd.deploy_params.copy()
+        atd.deploy_params = {'uniquename': 'testString',
+                             'sshKeyData': ssh_pub_key,
+                             'virtualNetworkResourceGroup': original_params['virtualNetworkResourceGroup'],
+                             'virtualNetworkName': original_params['virtualNetworkName'],
+                             'virtualNetworkSubnetName': original_params['virtualNetworkSubnetName'],
+                             'nfsCommaSeparatedAddresses': vserver_list,
+                             'vmCount': 12,
+                             'nfsExportPath': '/msazure',
+                             'bootstrapScriptPath': '/bootstrap/bootstrap.sh'
+                            }
+        wait_for_op(atd.deploy(add_secrets_params=False))
+    
 # FIXTURES ####################################################################
 @pytest.fixture(scope='class')
 def atd():
