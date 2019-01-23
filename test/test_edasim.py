5f52f14535731ebeb43affbddd4f5ad15637ad9b
rebecca-makar <remakar@microsoft.com>
2019-01-23 18:01:24 -0500

    fix edasim.py

--- test/test_edasim.py
+++ test/test_edasim.py
@@ -118,7 +118,7 @@ class TestEdasim:
     def test_edasim_deploy(self, test_vars, vs_ips, ssh_con):  # noqa: F811
         td = test_vars["atd_obj"]
         td.template = requests.get(
-                url=('https://raw.githubusercontent.com/Azure/Avere/master/src/go/cmd/edasim/deploymentartifacts/template/azuredeploy.json').json())
+                url='https://raw.githubusercontent.com/Azure/Avere/master/src/go/cmd/edasim/deploymentartifacts/template/azuredeploy.json').json()
         with open(os.path.expanduser(r'~/.ssh/id_rsa.pub'), 'r') as ssh_pub_f:
             ssh_pub_key = ssh_pub_f.read()
         command3 = command1 + command2
