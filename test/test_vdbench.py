ab1f6f2f6f863ba986aa09737745d8734611338b
rebecca-makar <remakar@microsoft.com>
2019-02-06 13:04:18 -0500

    vdbench

--- test/test_vdbench.py
+++ test/test_vdbench.py
@@ -25,7 +25,7 @@ class TestVDBench:
         commands = """
             sudo mkdir -p /nfs/node0/bootstrap
             cd /nfs/node0/bootstrap
-            sudo curl --retry 5 --retry-delay 5 -o /nfs/node0/bootstrap/bootstrap.vdbench.sh https://raw.githubusercontent.com/Azure/Avere/fix_vdbecnh/src/clientapps/vdbench/bootstrap.vdbench.sh
+            sudo curl --retry 5 --retry-delay 5 -o /nfs/node0/bootstrap/bootstrap.vdbench.sh https://raw.githubusercontent.com/Azure/Avere/master/src/clientapps/vdbench/bootstrap.vdbench.sh
             sudo curl --retry 5 --retry-delay 5 -o /nfs/node0/bootstrap/vdbench50407.zip https://avereimageswestus.blob.core.windows.net/vdbench/vdbench50407.zip
             sudo curl --retry 5 --retry-delay 5 -o /nfs/node0/bootstrap/vdbenchVerify.sh https://raw.githubusercontent.com/Azure/Avere/master/src/clientapps/vdbench/vdbenchVerify.sh
             sudo chmod +x /nfs/node0/bootstrap/vdbenchVerify.sh
