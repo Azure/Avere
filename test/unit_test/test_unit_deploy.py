dec73ff6cc847ec71b6f0d72cf849315bfd493a8
rebecca-makar <remakar@microsoft.com>
2019-02-28 11:23:11 -0500

    Add unit test and run in pipelines

--- test/unit_test/test_unit_deploy.py
+++ test/unit_test/test_unit_deploy.py
@@ -1,18 +1,17 @@
-import json
-import ast
+#!/usr/bin/python3
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE-CODE in the project root for license information.
+# import json
 import os
 from datetime import datetime
 from random import choice
 from string import ascii_lowercase
 from uuid import uuid4
 import unittest
+import json
 
 
-import jsonschema
-from jsonschema import validate
-
-
-class TestDeploy(unittest.TestCase):
+class TestUnitDeploy(unittest.TestCase):
     def setUp(
         self,
         deploy_id=None,
@@ -37,9 +36,9 @@ class TestDeploy(unittest.TestCase):
         if not self.resource_group:
             self.resource_group = self.deploy_id + "-rg"
 
-    def test_json(self):
+    def test_json_vfxt_deploy(self):
         # deploy template
-        with open("../../src/vfxt/azuredeploy-auto.json") as tfile:
+        with open("src/vfxt/azuredeploy-auto.json") as tfile:
             schema = json.load(tfile)
 
         # The data to be tested:
@@ -63,6 +62,81 @@ class TestDeploy(unittest.TestCase):
             "virtualNetworkSubnetName": self.deploy_id + "-subnet",
         }
 
+        testvalue = self.checkdata(schema, data)
+        self.assertTrue(testvalue)
+
+    def test_json_vfxt_negative_deploy(self):
+        # deploy template
+        with open("src/vfxt/azuredeploy-auto.json") as tfile:
+            schema = json.load(tfile)
+
+        # The data to be tested:
+        data = {
+            "adminPassword": os.environ["AVERE_ADMIN_PW"],
+            "avereBackedStorageAccountName": self.deploy_id + "sa",
+            "avereClusterName": self.deploy_id + "-cluster",
+            "avereClusterRole": "Avere Cluster Runtime Operator",
+            "avereInstanceType": "Standard_E32s_v3",
+            "avereNodeCount": False,
+            "controllerAdminUsername": "azureuser",
+            "controllerAuthenticationType": "sshPublicKey",
+            "controllerName": self.deploy_id + "-con",
+            "controllerPassword": os.environ["AVERE_CONTROLLER_PW"],
+            "controllerSSHKeyData": "string",
+            "enableCloudTraceDebugging": 2,
+            "rbacRoleAssignmentUniqueId": str(uuid4()),
+            "createVirtualNetwork": True,
+            "virtualNetworkName": self.deploy_id + "-vnet",
+            "virtualNetworkResourceGroup": self.resource_group,
+            "virtualNetworkSubnetName": self.deploy_id + "-subnet",
+        }
+
+        testvalue = self.checkdata(schema, data)
+        self.assertFalse(testvalue)
+
+    def test_json_vdbench_deploy(self):
+        # deploy template
+        with open("src/client/vmas/azuredeploy.json") as tfile:
+            schema = json.load(tfile)
+
+        # The data to be tested:
+        data = {
+            "uniquename": self.deploy_id,
+            "sshKeyData": "sshPublicKey",
+            "virtualNetworkResourceGroup": self.resource_group,
+            "virtualNetworkName": self.deploy_id + "-vnet",
+            "virtualNetworkSubnetName": self.deploy_id + "-subnet",
+            "nfsCommaSeparatedAddresses": "10.0.0.4, 10.0.0.5",
+            "vmCount": 12,
+            "nfsExportPath": "/msazure",
+            "bootstrapScriptPath": "/bootstrap/bootstrap.vdbench.sh",
+        }
+
+        testvalue = self.checkdata(schema, data)
+        self.assertTrue(testvalue)
+
+    def test_json_vdbench_negative_deploy(self):
+        # deploy template
+        with open("src/client/vmas/azuredeploy.json") as tfile:
+            schema = json.load(tfile)
+
+        # The data to be tested:
+        data = {
+            "uniquename": self.deploy_id,
+            "sshKeyData": 2,
+            "virtualNetworkResourceGroup": self.resource_group,
+            "virtualNetworkName": True,
+            "virtualNetworkSubnetName": self.deploy_id + "-subnet",
+            "nfsCommaSeparatedAddresses": "10.0.0.4, 10.0.0.5",
+            "vmCount": 12,
+            "nfsExportPath": "/msazure",
+            "bootstrapScriptPath": "/bootstrap/bootstrap.vdbench.sh",
+        }
+
+        testvalue = self.checkdata(schema, data)
+        self.assertFalse(testvalue)
+
+    def checkdata(self, schema, data):
         schema_types = {}
         for k, v in schema["parameters"].items():
             if v["type"] == "securestring" or v["type"] == "string":
@@ -71,11 +145,15 @@ class TestDeploy(unittest.TestCase):
                 type_str = v["type"]
             schema_types[k] = {"type": type_str}
 
+        testvalue = True
         for key, item in data.items():
-            if isinstance(item, eval(schema_types[key]["type"])):
-                pass
+            if isinstance(item, eval(schema_types[key]["type"])) or testvalue is False:
+                testvalue = True
             else:
-                self.fail("failure")
+                testvalue = False
+                break
+
+        return testvalue
 
 
 if __name__ == "__main__":
