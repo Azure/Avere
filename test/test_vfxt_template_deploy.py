f3b28d980fbcd409f2ae6b0e5b6f8e40e07be805
Omar Zevallos <45883639+omzevall@users.noreply.github.com>
2019-03-29 07:44:36 -0400

    Remove custom roles from ARM templates (#367)
    * Remove custom roles from ARM templates
    
    * Update tests to use default avereClusterRole when deploying

--- test/test_vfxt_template_deploy.py
+++ test/test_vfxt_template_deploy.py
@@ -40,7 +40,6 @@ class TestVfxtTemplateDeploy:
             "adminPassword": os.environ["AVERE_ADMIN_PW"],
             "avereBackedStorageAccountName": atd.deploy_id + "sa",
             "avereClusterName": atd.deploy_id + "-cluster",
-            "avereClusterRole": "Avere Cluster Runtime Operator",
             "avereInstanceType": "Standard_E32s_v3",
             "avereNodeCount": 3,
             "controllerAdminUsername": "azureuser",
@@ -93,7 +92,6 @@ class TestVfxtTemplateDeploy:
         atd.deploy_params = {
             "adminPassword": os.environ["AVERE_ADMIN_PW"],
             "avereClusterName": atd.deploy_id + "-cluster",
-            "avereClusterRole": "Avere Cluster Runtime Operator",
             "avereInstanceType": "Standard_E32s_v3",
             "avereNodeCount": 3,
             "controllerAdminUsername": "azureuser",
@@ -150,7 +148,6 @@ class TestVfxtTemplateDeploy:
             "adminPassword": os.environ["AVERE_ADMIN_PW"],
             "avereBackedStorageAccountName": atd.deploy_id + "sa",
             "avereClusterName": atd.deploy_id + "-cluster",
-            "avereClusterRole": "Avere Cluster Runtime Operator",
             "avereInstanceType": "Standard_E32s_v3",
             "avereNodeCount": 3,
             "controllerAdminUsername": "azureuser",
