42a6a0aebf6ee42d2bb4cba4545c6a64d07fe762
rebecca-makar <remakar@microsoft.com>
2019-04-24 14:54:43 -0400

    change sed commands

--- test/test_vfxt_client_docker.py
+++ test/test_vfxt_client_docker.py
@@ -36,7 +36,7 @@ class TestClientDocker:
             sudo sed -i -e 's/<dockerPassword>/{2}/g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
             sudo sed -i -e 's/<storageAcct>/{3}/g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
             sudo sed -i -e 's/<publicIp>/{4}/g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
-            sudo sed -i -e 's/<saKey>/{5}/g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
+            sudo sed -i -e 's:<saKey>:{5}:g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
             sudo sed -i -e 's/<clusterMgmt>/{6}/g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
             sudo sed -i -e 's/<avereAdminPw>/{7}/g' /nfs/node0/bootstrap/bootstrap.dockerfile.sh
             """.format(os.environ["dockerRegistry"], os.environ["dockerUsername"], os.environ["dockerPassword"], atd.deploy_id + "sa", test_vars["public_ip"], os.environ["SA_KEY"], cluster_mgmt_ip, os.environ["AVERE_ADMIN_PW"]).split("\n")
