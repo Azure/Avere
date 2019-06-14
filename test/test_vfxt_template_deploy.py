25f3bf7b15c7c0e7fd24c833907682d85f8d2d40
Omar Zevallos <45883639+omzevall@users.noreply.github.com>
2019-06-14 10:07:33 -0400

    Update envars for node image ref

--- test/test_vfxt_template_deploy.py
+++ test/test_vfxt_template_deploy.py
@@ -58,8 +58,8 @@ class TestVfxtTemplateDeploy:
 
         if "VFXT_CONTROLLER_IMG_REF_ID" in os.environ:
             atd.deploy_params["controllerImageReferenceId"] = os.environ["VFXT_CONTROLLER_IMG_REF_ID"]
-        if "NODE_IMAGE_SAS_URL" in os.environ:
-            atd.deploy_params["nodeImageId"] = os.environ["NODE_IMAGE_SAS_URL"]
+        if "VFXT_NODE_IMAGE_REF_ID" in os.environ:
+            atd.deploy_params["nodeImageId"] = os.environ["VFXT_NODE_IMAGE_REF_ID"]
 
         test_vars["storage_account"] = atd.deploy_params["avereBackedStorageAccountName"]
         test_vars["controller_name"] = atd.deploy_params["controllerName"]
@@ -118,8 +118,8 @@ class TestVfxtTemplateDeploy:
 
         if "VFXT_CONTROLLER_IMG_REF_ID" in os.environ:
             atd.deploy_params["controllerImageReferenceId"] = os.environ["VFXT_CONTROLLER_IMG_REF_ID"]
-        if "NODE_IMAGE_SAS_URL" in os.environ:
-            atd.deploy_params["nodeImageId"] = os.environ["NODE_IMAGE_SAS_URL"]
+        if "VFXT_NODE_IMAGE_REF_ID" in os.environ:
+            atd.deploy_params["nodeImageId"] = os.environ["VFXT_NODE_IMAGE_REF_ID"]
 
         test_vars["controller_name"] = atd.deploy_params["controllerName"]
         test_vars["controller_user"] = atd.deploy_params["controllerAdminUsername"]
@@ -176,8 +176,8 @@ class TestVfxtTemplateDeploy:
 
         if "VFXT_CONTROLLER_IMG_REF_ID" in os.environ:
             atd.deploy_params["controllerImageReferenceId"] = os.environ["VFXT_CONTROLLER_IMG_REF_ID"]
-        if "NODE_IMAGE_SAS_URL" in os.environ:
-            atd.deploy_params["nodeImageId"] = os.environ["NODE_IMAGE_SAS_URL"]
+        if "VFXT_NODE_IMAGE_REF_ID" in os.environ:
+            atd.deploy_params["nodeImageId"] = os.environ["VFXT_NODE_IMAGE_REF_ID"]
 
         test_vars["storage_account"] = atd.deploy_params["avereBackedStorageAccountName"]
         test_vars["controller_name"] = atd.deploy_params["controllerName"]
