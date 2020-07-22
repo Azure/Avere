761d2150d89e5d29f8163844edd0bb6a36d75c85
eoinbailey <baileye@gmail.com>
2020-07-22 11:03:46 +0100

    Update sample script to match PBRT location on render nodes

--- src/clientapps/opencue/README.md
+++ src/clientapps/opencue/README.md
@@ -1,3 +1,3 @@
 # OpenCue on Azure
 
-This template implements OpenCue.
+This template implements OpenCue RQD client onto the machine executing the script.
--- src/terraform/examples/vfxt/opencue/pre-existing-azure-blob/moanaislandscenescripts/renderAll.sh
+++ src/terraform/examples/vfxt/opencue/pre-existing-azure-blob/moanaislandscenescripts/renderAll.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# /datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island.png island.pbrt
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/islandX.png islandX.pbrt > islandX.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_beachCam.png island_beachCam.pbrt > island_beachCam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_dunesACam.png island_dunesACam.pbrt > island_dunesACam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_frame90.png island_frame90.pbrt > island_frame90.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_grassCam.png island_grassCam.pbrt > island_grassCam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_hibiscusCam.png island_hibiscusCam.pbrt > island_hibiscusCam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_palmsCam.png island_palmsCam.pbrt > island_palmsCam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_rootsCam.png island_rootsCam.pbrt > island_rootsCam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_shotCam.png island_shotCam.pbrt > island_shotCam.log
-/datadrive/pbrt/release/pbrt -outfile /home/AzureUser/island_4096.png island_4096.pbrt > island_4096.log
+/pbrt/release/pbrt -outfile /home/azureuser/island.png island.pbrt > island.log
+/pbrt/release/pbrt -outfile /home/azureuser/islandX.png islandX.pbrt > islandX.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_beachCam.png island_beachCam.pbrt > island_beachCam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_dunesACam.png island_dunesACam.pbrt > island_dunesACam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_frame90.png island_frame90.pbrt > island_frame90.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_grassCam.png island_grassCam.pbrt > island_grassCam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_hibiscusCam.png island_hibiscusCam.pbrt > island_hibiscusCam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_palmsCam.png island_palmsCam.pbrt > island_palmsCam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_rootsCam.png island_rootsCam.pbrt > island_rootsCam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_shotCam.png island_shotCam.pbrt > island_shotCam.log
+/pbrt/release/pbrt -outfile /home/azureuser/island_4096.png island_4096.pbrt > island_4096.log
