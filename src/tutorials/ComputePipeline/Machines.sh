0be5c7e23ccae3d53d26e3a73c6b95c9d14bb3b1
Rick Shahid <rick.shahid@live.com>
2020-09-25 07:05:27 -0700

    Simplified machine extension scripts

--- src/tutorials/ComputePipeline/Machines.sh
+++ src/tutorials/ComputePipeline/Machines.sh
@@ -2,13 +2,7 @@
 
 set -ex
 
-yum -y install https://downloads.teradici.com/rhel/teradici-repo-latest.noarch.rpm
-yum -y install epel-release
-yum -y install usb-vhci
-yum -y install pcoip-agent-graphics
-
 if [ "$TERADICI_LICENSE_KEY" != "" ]; then
     pcoip-register-host --registration-code="$TERADICI_LICENSE_KEY"
+    systemctl restart 'pcoip-agent'
 fi
-
-systemctl restart 'pcoip-agent'
