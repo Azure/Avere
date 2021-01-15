b6fe67faff0aed1bea2afdaa7967eed968ff57b4
Rebecca Makar <remakar@microsoft.com>
2021-01-15 15:00:42 -0500

    terraform rg

--- src/terraform/examples/resource-group/main.tf
+++ src/terraform/examples/resource-group/main.tf
@@ -6,8 +6,16 @@ variable "rg_location" {
   description = "resource group location."
   default = "eastus"
 }
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
 provider "azurerm" {
     version = "~>2.12.0"
+    subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
     features {}
 }
 resource "azurerm_resource_group" "rg" {
