cc6e5c60af010a15fda8812168ed40dfd930beb2
Anthony Howe <ahowe_ca@yahoo.ca>
2021-02-24 13:40:39 -0500

    update unbound

--- src/terraform/modules/dnsserveru/main.tf
+++ src/terraform/modules/dnsserveru/main.tf
@@ -147,6 +147,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
+    //publisher = "Canonical"
+    //offer     = "0001-com-ubuntu-server-focal"
+    //sku       = "20_04-lts"
+    //version   = "latest"
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
