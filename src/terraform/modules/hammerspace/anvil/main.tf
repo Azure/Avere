22551de482d7a5eebf51e127549b0de303c9fb29
Anthony Howe <ahowe_ca@yahoo.ca>
2021-04-23 19:46:00 -0400

    fix lb resource

--- src/terraform/modules/hammerspace/anvil/main.tf
+++ src/terraform/modules/hammerspace/anvil/main.tf
@@ -46,7 +46,6 @@ resource "azurerm_lb" "anvilloadbalancer" {
 // TODO - create the associations https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface_backend_address_pool_association
 resource "azurerm_lb_backend_address_pool" "anvilloadbalancerbepool" {
   count               = local.is_high_availability ? 1 : 0
-  resource_group_name = var.resource_group_name
   loadbalancer_id     = azurerm_lb.anvilloadbalancer[0].id
   name                = "${var.unique_name}LoadBalancerBEPool"
 }
