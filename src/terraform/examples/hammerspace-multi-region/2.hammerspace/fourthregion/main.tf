b8056c239bde80c0eaa4f6a57325cccff0868bfc
Anthony Howe <ahowe_ca@yahoo.ca>
2021-04-27 19:53:28 -0400

    update hammerspace examples

--- src/terraform/examples/hammerspace-multi-region/2.hammerspace/fourthregion/main.tf
+++ src/terraform/examples/hammerspace-multi-region/2.hammerspace/fourthregion/main.tf
@@ -25,51 +25,60 @@ locals {
   ad_user                = ""
   ad_password            = ""
   unique_name_4          = local.location4
-  use_highly_available   = false
-  anvil_configuration    = local.use_highly_available ? "High Availability" : "Standalone"
   // virtual network and subnet details
   data_subnet_mask_bits   = 25
   anvil_data_cluster_ip_4 = "10.3.2.240" // leave blank to be dynamic
   dsx_instance_count      = 1
 
-  // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
-  anvil_instance_type = "Standard_F8s_v2"
-  // anvil_instance_type = "Standard_F16s_v2"
-  // anvil_instance_type = "Standard_F32s_v2"
-  // anvil_instance_type = "Standard_F48s_v2"
-  //anvil_instance_type = "Standard_F16s_v2"
-  // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
-  dsx_instance_type = "Standard_F8s_v2"
-  // dsx_instance_type = "Standard_F16s_v2"
-  // dsx_instance_type = "Standard_F32s_v2"
-  // dsx_instance_type = "Standard_F48s_v2"
-  // dsx_instance_type = "Standard_F16s_v2"
+  region4_configuration = local.test_configuration
 
-  // storage_account_type = "Standard_LRS"
-  // storage_account_type = "StandardSSD_LRS"
-  storage_account_type = "Premium_LRS"
+  test_configuration = {
+    use_highly_available = false
+    // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
+    anvil_instance_type   = "Standard_F8s_v2"
+    metadata_disk_size_gb = 127
 
-  // more disk sizes and pricing found here: https://azure.microsoft.com/en-us/pricing/details/managed-disks/
-  // metadata_disk_size_gb = 127   //  P10, E10, S10
-  metadata_disk_size_gb = 255 //  P15, E15, S15
-  // metadata_disk_size_gb = 511   //  P20, E20, S20
-  // metadata_disk_size_gb = 1023  //  P30, E30, S30
-  // metadata_disk_size_gb = 2047  //  P40, E40, S40
-  // metadata_disk_size_gb = 4095  //  P50, E50, S50
-  // metadata_disk_size_gb = 8191  //  P60, E60, S60
-  // metadata_disk_size_gb = 16383 //  P70, E70, S70
-  // metadata_disk_size_gb = 32767 //  P80, E80, S80
+    dsx_instance_count = 1
+    // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
+    dsx_instance_type = "Standard_F8s_v2"
+    datadisk_size_gb  = 511
 
-  // more disk sizes and pricing found here: https://azure.microsoft.com/en-us/pricing/details/managed-disks/
-  // data_disk_size_gb = 127   //  P10, E10, S10
-  // data_disk_size_gb = 255   //  P15, E15, S15
-  // data_disk_size_gb = 511   //  P20, E20, S20
-  data_disk_size_gb = 1023 //  P30, E30, S30
-  // data_disk_size_gb = 2047  //  P40, E40, S40
-  //data_disk_size_gb = 4095  //  P50, E50, S50
-  // data_disk_size_gb = 8191  //  P60, E60, S60
-  // data_disk_size_gb = 16383 //  P70, E70, S70
-  // data_disk_size_gb = 32767 //  P80, E80, S80
+    storage_account_type = "Standard_LRS"
+    // storage_account_type = "StandardSSD_LRS"
+    // storage_account_type = "Premium_LRS"
+  }
+
+  artist_configuration = {
+    use_highly_available = false
+    // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
+    anvil_instance_type = "Standard_F16s_v2"
+    dsx_instance_count  = 3
+
+    // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
+    dsx_instance_type     = "Standard_DS14_v2"
+    metadata_disk_size_gb = 256
+    datadisk_size_gb      = 1024
+
+    // storage_account_type = "Standard_LRS"
+    // storage_account_type = "StandardSSD_LRS"
+    storage_account_type = "Premium_LRS"
+  }
+
+  render_configuration = {
+    use_highly_available = false
+    // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
+    anvil_instance_type   = "Standard_F16s_v2"
+    metadata_disk_size_gb = 256
+
+    dsx_instance_count = 3
+    // More sizes found here: https://docs.microsoft.com/en-us/azure/virtual-machines/sizes
+    dsx_instance_type = "Standard_L32s_v2"
+    datadisk_size_gb  = 0
+
+    // storage_account_type = "Standard_LRS"
+    // storage_account_type = "StandardSSD_LRS"
+    storage_account_type = "Premium_LRS"
+  }
 
   # advanced scenario: add external ports to work with cloud policies example [10022, 13389]
   open_external_ports = [22, 3389]
@@ -113,23 +122,25 @@ data "azurerm_storage_account" "sharedstorageaccount" {
 
 // the ephemeral filer
 module "anvil4" {
-  source                                = "github.com/Azure/Avere/src/terraform/modules/hammerspace/anvil"
-  resource_group_name                   = azurerm_resource_group.nfsfiler4.name
-  location                              = azurerm_resource_group.nfsfiler4.location
-  hammerspace_image_id                  = local.hammerspace_image_id_4
-  unique_name                           = local.unique_name_4
-  admin_username                        = local.admin_username
-  admin_password                        = local.admin_password
-  anvil_configuration                   = local.anvil_configuration
-  anvil_instance_type                   = local.anvil_instance_type
+  source               = "github.com/Azure/Avere/src/terraform/modules/hammerspace/anvil"
+  resource_group_name  = azurerm_resource_group.nfsfiler4.name
+  location             = azurerm_resource_group.nfsfiler4.location
+  hammerspace_image_id = local.hammerspace_image_id_4
+  unique_name          = local.unique_name_4
+  admin_username       = local.admin_username
+  admin_password       = local.admin_password
+
+  anvil_configuration              = local.region4_configuration.use_highly_available ? "High Availability" : "Standalone"
+  anvil_instance_type              = local.region4_configuration.anvil_instance_type
+  anvil_metadata_disk_size         = local.region4_configuration.metadata_disk_size_gb
+  anvil_metadata_disk_storage_type = local.region4_configuration.storage_account_type
+
   virtual_network_resource_group        = local.network_rg4_name
   virtual_network_name                  = local.network-region4-vnet_name
   virtual_network_ha_subnet_name        = local.network-region4-cloud_filers_ha_subnet_name
   virtual_network_data_subnet_name      = local.network-region4-cloud_filers_subnet_name
   virtual_network_data_subnet_mask_bits = local.data_subnet_mask_bits
   anvil_data_cluster_ip                 = local.anvil_data_cluster_ip_4
-  anvil_metadata_disk_storage_type      = local.storage_account_type
-  anvil_metadata_disk_size              = local.metadata_disk_size_gb
 
   depends_on = [
     azurerm_resource_group.nfsfiler4,
@@ -138,15 +149,19 @@ module "anvil4" {
 
 // the ephemeral filer
 module "dsx4" {
-  source                                = "github.com/Azure/Avere/src/terraform/modules/hammerspace/dsx"
-  resource_group_name                   = azurerm_resource_group.nfsfiler4.name
-  location                              = azurerm_resource_group.nfsfiler4.location
-  hammerspace_image_id                  = local.hammerspace_image_id_4
-  unique_name                           = local.unique_name_4
-  admin_username                        = local.admin_username
-  admin_password                        = local.admin_password
-  dsx_instance_count                    = local.dsx_instance_count
-  dsx_instance_type                     = local.dsx_instance_type
+  source               = "github.com/Azure/Avere/src/terraform/modules/hammerspace/dsx"
+  resource_group_name  = azurerm_resource_group.nfsfiler4.name
+  location             = azurerm_resource_group.nfsfiler4.location
+  hammerspace_image_id = local.hammerspace_image_id_4
+  unique_name          = local.unique_name_4
+  admin_username       = local.admin_username
+  admin_password       = local.admin_password
+
+  dsx_instance_count         = local.region4_configuration.dsx_instance_count
+  dsx_instance_type          = local.region4_configuration.dsx_instance_type
+  dsx_data_disk_size         = local.region4_configuration.datadisk_size_gb
+  dsx_data_disk_storage_type = local.region4_configuration.storage_account_type
+
   virtual_network_resource_group        = local.network_rg4_name
   virtual_network_name                  = local.network-region4-vnet_name
   virtual_network_data_subnet_name      = local.network-region4-cloud_filers_subnet_name
@@ -154,8 +169,6 @@ module "dsx4" {
   anvil_password                        = module.anvil4.web_ui_password
   anvil_data_cluster_ip                 = module.anvil4.anvil_data_cluster_ip
   anvil_domain                          = module.anvil4.anvil_domain
-  dsx_data_disk_storage_type            = local.storage_account_type
-  dsx_data_disk_size                    = local.data_disk_size_gb
 }
 
 module "anvil_configure4" {
