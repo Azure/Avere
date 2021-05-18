7931348553bbf57eecec1412066317750dcea676
Rick Shahid <rick.shahid@live.com>
2021-05-18 03:10:57 -0700

    Add storage download SAS querystring parameters (#1200)

--- src/terraform/modules/windowsgridgpu/variables.tf
+++ src/terraform/modules/windowsgridgpu/variables.tf
@@ -75,12 +75,12 @@ variable "install_pcoip" {
 
 variable "grid_url" {
   description = "specifies the grid url"
-  default     = "https://bit1.blob.core.windows.net/bin/Graphics/Windows/461.09_grid_win10_server2016_server2019_64bit_azure_swl.exe"
+  default     = "https://bit1.blob.core.windows.net/bin/Graphics/Windows/461.09_grid_win10_server2016_server2019_64bit_azure_swl.exe?sv=2020-04-08&st=2021-05-16T17%3A37%3A25Z&se=2222-05-17T17%3A37%3A00Z&sr=c&sp=rl&sig=jY6xDzLXfDogsXIAfwNMd5hCu%2BcR8Tg1rgJZreBFJj4%3D"
 }
 
 variable "teradici_pcoipagent_url" {
   description = "specifies the teradici pcoipagent"
-  default     = "https://bit1.blob.core.windows.net/bin/Teradici/pcoip-agent-graphics_21.03.0.exe"
+  default     = "https://bit1.blob.core.windows.net/bin/Teradici/pcoip-agent-graphics_21.03.0.exe?sv=2020-04-08&st=2021-05-16T17%3A37%3A25Z&se=2222-05-17T17%3A37%3A00Z&sr=c&sp=rl&sig=jY6xDzLXfDogsXIAfwNMd5hCu%2BcR8Tg1rgJZreBFJj4%3D"
 }
 
 variable "teradici_license_key" {
