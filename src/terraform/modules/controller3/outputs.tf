8f852b76f4d3db8ed727becd642823dad4b228dd
Anthony Howe <ahowe_ca@yahoo.ca>
2021-07-10 08:38:26 -0400

    add controller private ip address

--- src/terraform/modules/controller3/outputs.tf
+++ src/terraform/modules/controller3/outputs.tf
@@ -2,6 +2,10 @@ output "controller_address" {
   value = var.deploy_controller ? (var.add_public_ip ? azurerm_public_ip.vm[0].ip_address : azurerm_network_interface.vm[0].ip_configuration[0].private_ip_address) : ""
 }
 
+output "controller_private_address" {
+  value = var.deploy_controller ? azurerm_network_interface.vm[0].ip_configuration[0].private_ip_address : ""
+}
+
 output "controller_username" {
   value = var.admin_username
 }
