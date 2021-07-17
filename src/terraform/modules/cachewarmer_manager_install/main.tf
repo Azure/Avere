d751ff03ac0813209d561079260c7a6ee1e26227
Anthony Howe <ahowe_ca@yahoo.ca>
2021-07-17 12:33:36 -0400

    update the cachewarmer manager install

--- src/terraform/modules/cachewarmer_manager_install/main.tf
+++ src/terraform/modules/cachewarmer_manager_install/main.tf
@@ -1,29 +1,24 @@
 locals {
   mount_dir               = "/b"
-  bootstrap_dir           = "bootstrap"
   vmss_password_str       = var.vmss_password == null ? "" : var.vmss_password
   vmss_ssh_public_key_str = var.vmss_ssh_public_key == null ? "" : var.vmss_ssh_public_key
   vmss_subnet_name_str    = var.vmss_subnet_name == null ? "" : var.vmss_subnet_name
-  manager_bootstrap_path  = "/${local.bootstrap_dir}/bootstrap.cachewarmer-manager.sh"
-  env_vars                = "export BOOTSTRAP_PATH=${local.mount_dir} && export STORAGE_ACCOUNT=${var.storage_account} && export STORAGE_KEY='${var.storage_key}' && export QUEUE_PREFIX=${var.queue_name_prefix} && export BOOTSTRAP_EXPORT_PATH=${var.bootstrap_export_path} && export BOOTSTRAP_MOUNT_ADDRESS=${var.bootstrap_mount_address} && export BOOTSTRAP_SCRIPT=${var.bootstrap_worker_script_path} && export VMSS_USERNAME=${var.vmss_user_name} && export VMSS_SSHPUBLICKEY='${local.vmss_ssh_public_key_str}' && export VMSS_PASSWORD='${local.vmss_password_str}' && export VMSS_SUBNET=${local.vmss_subnet_name_str}"
+  env_vars                = "export BOOTSTRAP_PATH=${local.mount_dir} && export STORAGE_ACCOUNT_RESOURCE_GROUP='${var.storage_account_rg}' && export STORAGE_ACCOUNT=${var.storage_account} && export QUEUE_PREFIX=${var.queue_name_prefix} && export BOOTSTRAP_EXPORT_PATH=${var.bootstrap_export_path} && export BOOTSTRAP_MOUNT_ADDRESS=${var.bootstrap_mount_address} && export BOOTSTRAP_SCRIPT=${var.bootstrap_worker_script_path} && export VMSS_USERNAME=${var.vmss_user_name} && export VMSS_SSHPUBLICKEY='${local.vmss_ssh_public_key_str}' && export VMSS_PASSWORD='${local.vmss_password_str}' && export VMSS_SUBNET=${local.vmss_subnet_name_str}"
 }
 
 resource "null_resource" "install_cachewarmer_manager" {
-  count = var.deploy_cachewarmer ? 1 : 0
-
   connection {
     type        = "ssh"
-    port        = var.ssh_port
-    host        = var.node_address
-    user        = var.admin_username
-    password    = var.ssh_key_data != null && var.ssh_key_data != "" ? "" : var.admin_password
-    private_key = var.ssh_key_data != null && var.ssh_key_data != "" ? file("~/.ssh/id_rsa") : null
+    port        = var.jumpbox_ssh_port
+    host        = var.jumpbox_address
+    user        = var.jumpbox_username
+    password    = var.jumpbox_ssh_key_data != null && var.jumpbox_ssh_key_data != "" ? "" : var.jumpbox_password
+    private_key = var.jumpbox_ssh_key_data != null && var.jumpbox_ssh_key_data != "" ? file("~/.ssh/id_rsa") : null
   }
 
   provisioner "remote-exec" {
     inline = [
       "set -x",
-      "if [ -f '/etc/centos-release' ]; then sudo yum -y install git nfs-utils ; else sudo apt-get install -y nfs-common ; fi",
       "sudo mkdir -p ${local.mount_dir}",
       "sudo mount -o 'hard,nointr,proto=tcp,mountproto=tcp,retry=30' ${var.bootstrap_mount_address}:${var.bootstrap_export_path} ${local.mount_dir}",
       "${local.env_vars} && sudo -E /bin/bash ${local.mount_dir}${var.bootstrap_manager_script_path}",
