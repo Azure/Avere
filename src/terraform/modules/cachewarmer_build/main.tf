c296dfab76f992bdf46124a1c9887b1d1ea5097e
Anthony Howe <ahowe_ca@yahoo.ca>
2021-07-17 10:52:31 -0400

    update the cachewarmer prepare module

--- src/terraform/modules/cachewarmer_build/main.tf
+++ src/terraform/modules/cachewarmer_build/main.tf
@@ -1,8 +1,17 @@
 locals {
-  mount_dir              = "/b"
-  bootstrap_dir          = "bootstrap"
-  manager_bootstrap_path = "/${local.bootstrap_dir}/bootstrap.cachewarmer-manager.sh"
-  worker_bootstrap_path  = "/${local.bootstrap_dir}/bootstrap.cachewarmer-worker.sh"
+  build_cachewarmer_lines = [
+    "curl --retry 5 --retry-delay 5 -o /tmp/cachewarmer_build.sh https://raw.githubusercontent.com/Azure/Avere/main/src/terraform/modules/cachewarmer_build/cachewarmer_build.sh",
+    "source /tmp/cachewarmer_build.sh",
+  ]
+
+  env_vars = "LOCAL_MOUNT_DIR=/b BOOTSTRAP_MOUNT_ADDRESS=${var.bootstrap_mount_address} BOOTSTRAP_MOUNT_EXPORT=${var.bootstrap_export_path} BOOTSTRAP_SUBDIR=${var.bootstrap_subdir}"
+
+  prepare_cachewarmer_bootstrap_lines = [
+    "curl --retry 5 --retry-delay 5 -o /tmp/cachewarmer_prepare_bootstrap.sh https://raw.githubusercontent.com/Azure/Avere/main/src/terraform/modules/cachewarmer_build/cachewarmer_prepare_bootstrap.sh",
+    "${local.env_vars} source /tmp/cachewarmer_prepare_bootstrap.sh"
+  ]
+
+  provisioner_lines = var.build_cachewarmer ? concat(build_cachewarmer_lines, prepare_cachewarmer_bootstrap_lines) : prepare_cachewarmer_bootstrap_lines
 }
 
 resource "null_resource" "build_cachewarmer_bootstrap" {
@@ -10,48 +19,14 @@ resource "null_resource" "build_cachewarmer_bootstrap" {
 
   connection {
     type        = "ssh"
-    port        = var.ssh_port
-    host        = var.node_address
-    user        = var.admin_username
-    password    = var.ssh_key_data != null && var.ssh_key_data != "" ? "" : var.admin_password
-    private_key = var.ssh_key_data != null && var.ssh_key_data != "" ? file("~/.ssh/id_rsa") : null
+    port        = var.jumpbox_ssh_port
+    host        = var.jumpbox_address
+    user        = var.jumpbox_username
+    password    = var.jumpbox_ssh_key_data != null && var.jumpbox_ssh_key_data != "" ? "" : var.jumpbox_password
+    private_key = var.jumpbox_ssh_key_data != null && var.jumpbox_ssh_key_data != "" ? file("~/.ssh/id_rsa") : null
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "set -x",
-      "if [ -f '/etc/centos-release' ]; then sudo yum -y install git nfs-utils ; else sudo apt-get install -y nfs-common ; fi",
-      "wget -O ~/go.tgz https://golang.org/dl/go1.14.4.linux-amd64.tar.gz",
-      "tar zxf ~/go.tgz",
-      "sudo chown -R root:root ~/go",
-      "sudo rm -rf /usr/local/go",
-      "sudo mv go /usr/local",
-      "rm ~/go.tgz",
-      "rm -rf ~/gopath",
-      "mkdir ~/gopath",
-      "sudo mkdir -p ${local.mount_dir}",
-      "sudo mount -o 'hard,nointr,proto=tcp,mountproto=tcp,retry=30' ${var.bootstrap_mount_address}:${var.bootstrap_export_path} ${local.mount_dir}",
-      "sudo mkdir -p ${local.mount_dir}/${local.bootstrap_dir}",
-      "sudo mkdir -p ${local.mount_dir}/${local.bootstrap_dir}/cachewarmerbin",
-      "echo \"export GOPATH=$HOME/gopath\" >> ~/.profile",
-      "echo \"export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin\" >> ~/.profile",
-      "/bin/bash -c 'source ~/.profile && cd $GOPATH && go get github.com/Azure/Avere/src/go/... && sudo cp $GOPATH/bin/cachewarmer-* ${local.mount_dir}/${local.bootstrap_dir}/cachewarmerbin'",
-      "sudo mkdir -p ${local.mount_dir}/${local.bootstrap_dir}/rsyslog",
-      "sudo mkdir -p ${local.mount_dir}/${local.bootstrap_dir}/systemd",
-      "sudo rm -f ${local.mount_dir}${local.manager_bootstrap_path}",
-      "sudo rm -f ${local.mount_dir}${local.worker_bootstrap_path}",
-      "sudo rm -f ${local.mount_dir}/${local.bootstrap_dir}/rsyslog/35-cachewarmer-manager.conf",
-      "sudo rm -f ${local.mount_dir}/${local.bootstrap_dir}/rsyslog/36-cachewarmer-worker.conf",
-      "sudo rm -f ${local.mount_dir}/${local.bootstrap_dir}/systemd/cachewarmer-manager.service",
-      "sudo rm -f ${local.mount_dir}/${local.bootstrap_dir}/systemd/cachewarmer-worker.service",
-      "sudo curl --retry 5 --retry-delay 5 -o ${local.mount_dir}${local.manager_bootstrap_path} https://raw.githubusercontent.com/Azure/Avere/main/src/go/cmd/cachewarmer/deploymentartifacts/bootstrap/bootstrap.cachewarmer-manager.sh",
-      "sudo curl --retry 5 --retry-delay 5 -o ${local.mount_dir}${local.worker_bootstrap_path} https://raw.githubusercontent.com/Azure/Avere/main/src/go/cmd/cachewarmer/deploymentartifacts/bootstrap/bootstrap.cachewarmer-worker.sh",
-      "sudo curl --retry 5 --retry-delay 5 -o ${local.mount_dir}/${local.bootstrap_dir}/rsyslog/35-cachewarmer-manager.conf https://raw.githubusercontent.com/Azure/Avere/main/src/go/cmd/cachewarmer/deploymentartifacts/bootstrap/rsyslog/35-cachewarmer-manager.conf",
-      "sudo curl --retry 5 --retry-delay 5 -o ${local.mount_dir}/${local.bootstrap_dir}/rsyslog/36-cachewarmer-worker.conf https://raw.githubusercontent.com/Azure/Avere/main/src/go/cmd/cachewarmer/deploymentartifacts/bootstrap/rsyslog/36-cachewarmer-worker.conf",
-      "sudo curl --retry 5 --retry-delay 5 -o ${local.mount_dir}/${local.bootstrap_dir}/systemd/cachewarmer-manager.service https://raw.githubusercontent.com/Azure/Avere/main/src/go/cmd/cachewarmer/deploymentartifacts/bootstrap/systemd/cachewarmer-manager.service",
-      "sudo curl --retry 5 --retry-delay 5 -o ${local.mount_dir}/${local.bootstrap_dir}/systemd/cachewarmer-worker.service https://raw.githubusercontent.com/Azure/Avere/main/src/go/cmd/cachewarmer/deploymentartifacts/bootstrap/systemd/cachewarmer-worker.service",
-      "sudo umount ${local.mount_dir}",
-      "sudo rmdir ${local.mount_dir}",
-    ]
+    inline = local.provisioner_lines
   }
 }
--- src/terraform/modules/cachewarmer_build/variables.tf
+++ src/terraform/modules/cachewarmer_build/variables.tf
@@ -1,28 +1,23 @@
-variable "deploy_cachewarmer" {
-  description = "specifies to create the cachewarmer or not"
-  default     = true
-}
-
-variable "node_address" {
+variable "jumpbox_address" {
   description = "The address of controller or jumpbox"
 }
 
-variable "admin_username" {
-  description = "Admin username on the controller or jumpbox"
+variable "jumpbox_username" {
+  description = "The username on the controller or jumpbox"
   default     = "azureuser"
 }
 
-variable "admin_password" {
+variable "jumpbox_password" {
   description = "(optional) The password used for access to the controller or jumpbox.  If not specified, ssh_key_data needs to be set."
   default     = null
 }
 
-variable "ssh_key_data" {
+variable "jumpbox_ssh_key_data" {
   description = "(optional) The public SSH key used for access to the controller or jumpbox.  If not specified, the password needs to be set.  The ssh_key_data takes precedence over the password, and if set, the password will be ignored."
   default     = null
 }
 
-variable "ssh_port" {
+variable "jumpbox_ssh_port" {
   description = "specifies the tcp port to use for ssh"
   default     = 22
 }
@@ -33,4 +28,15 @@ variable "bootstrap_mount_address" {
 
 variable "bootstrap_export_path" {
   description = "the export path that hosts the worker bootstrap script"
+}
+
+variable "bootstrap_subdir" {
+  description = "the subdirectory containing the cachewarmer bootstrap scripts"
+  default     = "/bootstrap"
+}
+
+variable "build_cachewarmer" {
+  description = "specify to build the cachewarmer, otherwise it will be downloaded from the release site"
+  type        = bool
+  default     = false
 }
