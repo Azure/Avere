7dc49adfc34a5b9707c7acbab1c4a7e90f19891b
Anthony Howe <anhowe@users.noreply.github.com>
2021-07-19 18:38:56 -0400

    add peering option to network (#1310)

--- src/terraform/modules/render_network/main.tf
+++ src/terraform/modules/render_network/main.tf
@@ -85,12 +85,55 @@ resource "azurerm_virtual_network" "vnet" {
   }
 }
 
+////////////////////////////////////////////////////////////////
+// peer the networks
+////////////////////////////////////////////////////////////////
+data "azurerm_virtual_network" "onprem" {
+  count               = var.peer_vnet_rg == null || var.peer_vnet_rg == "" || var.peer_vnet_name == null || var.peer_vnet_name == "" ? 0 : 1
+  name                = var.peer_vnet_name
+  resource_group_name = var.peer_vnet_rg
+}
+
+resource "azurerm_virtual_network_peering" "peer-to-onprem" {
+  count                     = var.peer_vnet_rg == null || var.peer_vnet_rg == "" || var.peer_vnet_name == null || var.peer_vnet_name == "" ? 0 : 1
+  name                      = "peertoonprem"
+  resource_group_name       = azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = data.azurerm_virtual_network.onprem.id
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+  ]
+}
+
+resource "azurerm_virtual_network_peering" "peer-from-onprem" {
+  count                     = var.peer_vnet_rg == null || var.peer_vnet_rg == "" || var.peer_vnet_name == null || var.peer_vnet_name == "" ? 0 : 1
+  name                      = "peerfromonprem"
+  resource_group_name       = var.peer_vnet_rg
+  virtual_network_name      = var.peer_vnet_name
+  remote_virtual_network_id = azurerm_virtual_network.vnet.id
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+  ]
+}
+
+////////////////////////////////////////////////////////////////
+// subnets
+////////////////////////////////////////////////////////////////
+
 resource "azurerm_subnet" "cloud_cache" {
   name                 = var.subnet_cloud_cache_subnet_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = [var.subnet_cloud_cache_address_prefix]
   service_endpoints    = ["Microsoft.Storage"]
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    resource.azurerm_virtual_network_peering.peer-to-onprem,
+    azurerm_virtual_network_peering, peer-from-onprem,
+  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "cloud_cache" {
@@ -103,6 +146,12 @@ resource "azurerm_subnet" "cloud_filers" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = [var.subnet_cloud_filers_address_prefix]
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    resource.azurerm_virtual_network_peering.peer-to-onprem,
+    azurerm_virtual_network_peering, peer-from-onprem,
+  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "cloud_filers" {
@@ -115,6 +164,12 @@ resource "azurerm_subnet" "cloud_filers_ha" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = [var.subnet_cloud_filers_ha_address_prefix]
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    resource.azurerm_virtual_network_peering.peer-to-onprem,
+    azurerm_virtual_network_peering, peer-from-onprem,
+  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "cloud_filers_ha" {
@@ -129,6 +184,12 @@ resource "azurerm_subnet" "jumpbox" {
   address_prefixes     = [var.subnet_jumpbox_address_prefix]
   # needed for the controller to add storage containers
   service_endpoints = ["Microsoft.Storage"]
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    resource.azurerm_virtual_network_peering.peer-to-onprem,
+    azurerm_virtual_network_peering, peer-from-onprem,
+  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "jumpbox" {
@@ -141,6 +202,12 @@ resource "azurerm_subnet" "render_clients1" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = [var.subnet_render_clients1_address_prefix]
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    resource.azurerm_virtual_network_peering.peer-to-onprem,
+    azurerm_virtual_network_peering, peer-from-onprem,
+  ]
 }
 
 // partition the render clients in groups of roughly 500 nodes (max 507, and azure takes 5 reserved)
@@ -154,6 +221,12 @@ resource "azurerm_subnet" "render_clients2" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.resource_group_name
   address_prefixes     = [var.subnet_render_clients2_address_prefix]
+
+  depends_on = [
+    azurerm_virtual_network.vnet,
+    resource.azurerm_virtual_network_peering.peer-to-onprem,
+    azurerm_virtual_network_peering, peer-from-onprem,
+  ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "render_clients2" {
