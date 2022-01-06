92ee21276be47510c94b157de9defa6e901aac72
Rick Shahid <rick.shahid@live.com>
2022-01-05 17:52:01 -0800

    Update end-to-end rendering deployment example

--- src/terraform/examples/e2e/6.compute.farm/terminate.ps1
+++ src/terraform/examples/e2e/6.compute.farm/terminate.ps1
@@ -1,8 +1,12 @@
 $scheduledEvents = (Invoke-RestMethod -Headers @{"Metadata"="true"} -Method Get -Uri "http://169.254.169.254/metadata/scheduledevents?api-version=2020-07-01").Events
 foreach ($scheduledEvent in $scheduledEvents) {
-  Write-Host $scheduledEvent
   $eventType = $scheduledEvent.EventType
   if ($eventType -eq "Preempt" -or $eventType -eq "Terminate") {
-    # Add efficient (time-limited) clean up code here as needed
+    $eventScope = $scheduledEvent.Resources
+    $instanceName = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method Get -Uri "http://169.254.169.254/metadata/instance/compute/name?api-version=2021-05-01&format=text"
+    if ($eventScope -contains $instanceName) {
+      deadlineworker -shutdown
+      deadlinecommand -DeleteSlave $(hostname)
+    }
   }
 }
