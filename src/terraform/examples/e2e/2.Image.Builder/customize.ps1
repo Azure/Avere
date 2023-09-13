5bb979cacf87dee8e593813655e85dd72c87ce87
Rick Shahid <rick.shahid@live.com>
2023-09-13 12:52:15 -0700

    Azure rendering solution deployment framework

--- src/terraform/examples/e2e/2.Image.Builder/customize.ps1
+++ src/terraform/examples/e2e/2.Image.Builder/customize.ps1
@@ -8,24 +8,7 @@ $binPaths = ""
 $binDirectory = "C:\Users\Public\Downloads"
 Set-Location -Path $binDirectory
 
-function StartProcess ($filePath, $argumentList, $logFile) {
-  if ($logFile -eq $null) {
-    if ($argumentList -eq $null) {
-      Start-Process -FilePath $filePath -Wait
-    } else {
-      Start-Process -FilePath $filePath -ArgumentList $argumentList -Wait
-    }
-  } else {
-    if ($argumentList -eq $null) {
-      Start-Process -FilePath $filePath -Wait -RedirectStandardError $logFile-err.log -RedirectStandardOutput $logFile-out.log
-    } else {
-      Start-Process -FilePath $filePath -ArgumentList $argumentList -Wait -RedirectStandardError $logFile-err.log -RedirectStandardOutput $logFile-out.log
-    }
-    Get-Content -Path $logFile-err.log | Tee-Object -FilePath "$logFile.log" -Append
-    Get-Content -Path $logFile-out.log | Tee-Object -FilePath "$logFile.log" -Append
-    Remove-Item -Path $logFile-err.log, $logFile-out.log
-  }
-}
+. "C:\AzureData\functions.ps1"
 
 Write-Host "Customize (Start): Resize OS Disk"
 $osDriveLetter = "C"
